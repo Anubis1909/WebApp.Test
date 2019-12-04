176bf5a00a5a1ea8887ef26b56851395b650aedd
Marko Jovanovic <anubis1909@yahoo.com>
2019-12-04 20:29:18 +0100

    Dodata kalsa i kontroler

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using People;
+using Microsoft.CSharp;
+using Microsoft.AspNet.Mvc;
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class PeopleController
+{
+	public IActionResult Index()
+	{
+		
+		return View();
+	}
+}
---
+++ People.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class People
+{
+	public People()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public int Id { get; set; }
+    public string  FirstName { get; set; }
+    public string LastName { get; set; }
+
+}
